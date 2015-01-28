4b748103f77c3fee875c99b0e73abc7a8f42463c
William Narmontas <william@scalawilliam.com>
2015-01-28 15:16:18 +0000

    Added user name display support, yay!

--- frontend/app/plugins/ServerUpdatesPlugin.scala
+++ frontend/app/plugins/ServerUpdatesPlugin.scala
@@ -2,6 +2,7 @@ package plugins
 
 import akka.util.Timeout
 import com.hazelcast.core.{Message, MessageListener, Hazelcast}
+import org.json4s.jackson.Serialization._
 import play.api._
 import play.api.libs.concurrent.Akka
 import plugins.ServerUpdatesPlugin._
@@ -15,28 +16,57 @@ class ServerUpdatesPlugin(implicit app: Application) extends Plugin {
   lazy val currentState = scala.collection.mutable.Map.empty[String, String]
   var currentStateJson: String = "[]"
   implicit lazy val system = Akka.system
+
+  def enrichUsers(json: String): Future[String] = {
+    import scala.concurrent.ExecutionContext.Implicits.global
+    BasexProviderPlugin.awaitPlugin.query(<rest:query xmlns:rest="http://basex.org/rest">
+      <rest:text><![CDATA[
+declare option output:method 'json';
+declare variable $json as xs:string external;
+
+let $jenny :=
+  copy $j := json:parse($json)
+  modify (
+    for $player in $j//players/_
+    return (
+      for $ip in $player/ip
+      return delete node $ip,
+      for $name-node in $player/name
+      let $name := data($name-node)
+      for $user in /registered-user[@game-nickname = $name]
+      return insert node <user>{data($user/@id)}</user> into $player
+    )
+  )
+  return $j
+return $jenny
+]]>
+      </rest:text>
+      <rest:variable name="json" value={json}/>
+    </rest:query>).map(_.body)
+  }
+
   lazy val act = actor(name="server-updates")(new Act {
+    case class EnrichedJson(serverName: String, data: String)
+    import scala.concurrent.ExecutionContext.Implicits.global
     become {
-      case GotUpdate(j) =>
-        import org.json4s._
-        import org.json4s.jackson.JsonMethods._
-        import org.json4s.jackson.Serialization
-        import org.json4s.jackson.Serialization.{read, write}
-        val cleanerJson = parse(j) removeField {
-          case ("ip", _)=> true
-          case other => false
-        }
-        import org.json4s.JsonDSL._
-        implicit val formats = Serialization.formats(NoTypeHints)
-        val serverName: String = (cleanerJson \ "now" \ "server" \ "server").extract[String]
-        val newJsonS = write(cleanerJson)
-        currentState += serverName -> newJsonS
+      case EnrichedJson(serverName, data) =>
+        currentState += serverName -> data
         currentStateJson = {
           import play.api.libs.json.{JsNull, Json, JsString, JsValue}
           val json = Json.toJson(currentState.valuesIterator.toList)
           json.toString()
         }
-        context.system.eventStream.publish(ServerState(serverName, newJsonS))
+        context.system.eventStream.publish(ServerState(serverName, data))
+      case GotUpdate(j) =>
+        import org.json4s._
+        import org.json4s.jackson.JsonMethods._
+        import org.json4s.jackson.Serialization
+        import org.json4s.jackson.Serialization.write
+        implicit val formats = Serialization.formats(NoTypeHints)
+        val cleanerJson = parse(j)
+        val serverName: String = (cleanerJson \ "now" \ "server" \ "server").extract[String]
+        import akka.pattern.pipe
+        enrichUsers(json = write(cleanerJson)).map(EnrichedJson(serverName, _)) pipeTo self
       case GiveStates =>
         sender() ! CurrentStates(currentState.toMap)
       case GiveStatesJson =>
