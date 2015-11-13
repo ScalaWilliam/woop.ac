c6fdfffa0331d2d7c9f20913d9e7e3026e1e7cce
William Narmontas <william@scalawilliam.com>
2015-11-13 21:19:04 +0000

    Remove frontend

--- frontend/app/assets/javascripts/serverclient.js
+++
@@ -1,8 +0,0 @@
-//(function() {
-//    var wsUri = "ws://127.0.0.1:9000/server-updates";
-//    var websocket = new WebSocket(wsUri);
-//    websocket.onopen = function(evt) { onOpen(evt) };
-//    websocket.onclose = function(evt) { onClose(evt) };
-//    websocket.onmessage = function(evt) { alert(evt.data) };
-//    websocket.onerror = function(evt) { onError(evt) };
-//})();
--- frontend/app/controllers/package.scala
+++
@@ -1,5 +0,0 @@
-import scala.concurrent.ExecutionContext
-
-package object controllers {
-  implicit def ctx: ExecutionContext = play.api.libs.concurrent.Execution.Implicits.defaultContext
-}
