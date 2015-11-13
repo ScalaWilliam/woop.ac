edcbedfe3c123864fc83edc0f09126d203c0fc56
William Narmontas <william@scalawilliam.com>
2015-11-13 19:30:15 +0000

    Send raw games too

--- api/app/controllers/ApiMain.scala
+++ api/app/controllers/ApiMain.scala
@@ -4,19 +4,33 @@ import java.io.File
 import javax.inject._
 
 import play.api.Configuration
+import play.api.libs.iteratee.Enumerator
 import play.api.libs.json.{JsArray, Json}
 import play.api.mvc.{Action, Controller}
 
+import scala.concurrent.ExecutionContext
+
 
 @Singleton
-class ApiMain @Inject()(configuration: Configuration) extends Controller {
+class ApiMain @Inject()(configuration: Configuration)
+                       (implicit executionContext: ExecutionContext)extends Controller {
+
   val file = new File(configuration.underlying.getString("af.games.path"))
+
   val games = scala.io.Source.fromFile(file).getLines.map(_.split("\t").toList).collect {
     case List(id, "GOOD", "", json) => id -> Json.parse(json)
   }.take(10).toList
 
+  val lines = scala.io.Source.fromFile(file).getLines.map(_.split("\t").toList).collect {
+    case List(id, "GOOD", _, json) => s"$id\t$json  "
+  }.take(10).toList
+
   def recent = Action {
     Ok(JsArray(games.map { case (_, json) => json }))
+  }
+
+  def raw = Action {
+    Ok.chunked(Enumerator.enumerate(lines).map(l => s"$l\n")).as("text/tab-separated-values")
   }
 
 }
